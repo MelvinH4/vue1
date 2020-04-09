5abf797171735d935aaea089e5d401d560123230
Melvin Hernandez <melvineduardo4@gmail.com>
2020-04-09 15:12:11 -0600

    Enviado datos del componente "HelloWorld" al Componente "VerComponente" con Props.

---
+++ src/components/VerComponente.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>{{MensajeNormal}}</h1>
+        <h1>{{titulo}}</h1>-<h1>{{clase}}</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'VerComponente',
+        props:['MensajeNormal','titulo','clase']
+    }
+</script>
+
+<style scoped>
+
+</style>
